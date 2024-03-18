199fc622e2697f10667fac10e1ff54f9f77eebdd
Hovo07 <hovotovmasyan007@gmail.com>
2024-03-18 17:36:32 +0400

    created js 1

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-let y = 15
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i = i + 1;
+// }
+
+// let y = 10;
+
+// while (y >= 1) {
+//   console.log(y);
+//   y = y - 1;
+// }
+
+// let x = 1;
+
+// while (x <= 10) {
+//   console.log(x);
+//   x = x + 2;
+// }
+
+// let z = 0;
+
+// let sum = 0;
+
+// while (z <= 9) {
+//   sum = sum + z;
+//   z = z + 1;
+// }
+
+// console.log(sum);
+
+let m = 10;
+
+let prod = 1;
+
+while (m <= 15) {
+  prod = prod * m;
+  m = m + 1;
+}
+
+console.log(prod);
